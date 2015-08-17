994adabd433e073413b630a2d07fe090c895fb72
Андрей Цяпа <drunaa@gmail.com>
2015-08-17 19:50:45 +0300

    Fix label size Fix name's register and trim extra spaces

--- Banzai3/SelectNameForm.cs
+++ Banzai3/SelectNameForm.cs
@@ -24,9 +24,10 @@ namespace Banzai3
             Localization.SetLocalName(btnCancel);
             foreach (var exist in crossNames)
             {
-                var label = new Label {Text = exist};
+                var label = new Label {Text = exist, AutoSize = true};
                 panelLibrary.Controls.Add(label);
             }
+            UpdateControls();
         }
 
         public static string SelectName(string dir, string name)
@@ -40,31 +41,40 @@ namespace Banzai3
             var res = form.ShowDialog();
             if (res != DialogResult.OK)
                 return null;
-            return form.controlName.Text;
+            return form.controlName.Text.Trim();
         }
 
         private void controlName_TextChanged(object sender, EventArgs e)
         {
-            //TODO this code block rename of saved cross, it will be unblocked
+            UpdateControls();
+        }
+
+        private void UpdateControls()
+        {
+            //TODO this code is blocking rename of saved cross, it will be fix to unblock
+            var textLower = controlName.Text.Trim().ToLower();
+            var nameLower = name.Trim().ToLower();
             foreach (var label in panelLibrary.Controls.OfType<Label>())
             {
-                label.ForeColor = (label.Text == name)
+                label.ForeColor = (label.Text.Trim().ToLower() == nameLower)
                     ? Color.DarkGreen
-                    : (label.Text != controlName.Text)
+                    : (label.Text.Trim().ToLower() != textLower)
                         ? SystemColors.WindowText
                         : Color.Red;
             }
-            var exist = crossNames.Contains(controlName.Text) && (controlName.Text != name);
+            var exist = crossNames.Any(n => textLower == n.Trim().ToLower()) && (textLower != nameLower);
             labelName.BackColor = exist ? SystemColors.Info : SystemColors.Window;
-            btnOk.Enabled = !exist && !string.IsNullOrEmpty(controlName.Text);
+            btnOk.Enabled = !exist && !string.IsNullOrEmpty(textLower);
         }
 
         private void btnOk_Click(object sender, EventArgs e)
         {
-            var text = controlName.Text;
-            if (string.IsNullOrEmpty(text))
+            var textLower = controlName.Text.Trim().ToLower();
+            if (string.IsNullOrEmpty(textLower))
                 return;
-            if (!crossNames.Contains(text) || (text == name))
+            var nameLower = name.Trim().ToLower();
+            var exist = crossNames.Any(n => textLower == n.Trim().ToLower()) && (textLower != nameLower);
+            if (!exist || (textLower == nameLower))
                 DialogResult = DialogResult.OK;
         }
     }
